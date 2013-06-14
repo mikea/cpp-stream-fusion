5100dab02cc1c4355a92fcac26af40b978ceb865
Mike Aizatsky <mike.aizatsky@gmail.com>
2013-06-14 00:14:34 -0700

    Using googletest in the test.

--- vector_test.cc
+++ vector_test.cc
@@ -1,8 +1,15 @@
+#include "gtest/gtest.h"
 #include "vector.h"
 
-int main(int argc, const char* argv[]) {
-	Vector a(1024), b(1024), c(1024);
-	auto d = a + b + c;
-	printf("Length: %f\n", d.length());
+TEST(VectorTest, SumAndLength) {
+  // TODO: assert that no temps were created.
+  Vector a(1024), b(1024), c(1024);
+  auto d = a + b + c;
+  EXPECT_EQ(0, d.length());
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
